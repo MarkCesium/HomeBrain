30f44b957afe2c884ac899842ea2879cf805519d
MarkCesium <cesiummark01@gmail.com>
2023-12-14 08:25:21 +0300

    add log out button, new one db query

--- images/php/curlDownload.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-proxyData="http://login:passwords@address:port"
-export http_proxy=$proxyData
-export https_proxy=$proxyData
-curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-exit 0
